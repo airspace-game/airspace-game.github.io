6fd9924e80023acf9083c40cb37cb7a8a76ba5be
Jon Ross <jonross.zlsa+gitlab@gmail.com>
2016-06-26 14:24:57 -0700

    Deploy to openscope.github.io

--- bundle.js
+++ bundle.js
@@ -10592,20 +10592,18 @@
 	
 	    this.aircraft = [];
 	
-	    this.initDemoAircraft();
+	    setInterval(util.withScope(this, this.initDemoAircraft), 2000);
 	  }
 	
 	  initDemoAircraft() {
 	
-	    function randrange() {
-	      return util.lerp(0, Math.random(), 1, -0.2, 0.2);
-	    }
-	    
-	    for(var i=0; i<500; i++) {
+	    if(this.aircraft.length > 1000) return;
+	
+	    for(var i=0; i<1; i++) {
 	      this.addAircraft(new aircraft.Aircraft({
 	        callsign: 'VX0001',
 	        model: 'foo',
-	        position: [-122.366978 + randrange(), 37.627525 + randrange()],
+	        position: [-122.366978, 37.627525],
 	        altitude: util.lerp(0, Math.random(), 1, 100, 10000),
 	        heading: util.lerp(0, Math.random(), 1, 0, Math.PI * 2)
 	      }));
